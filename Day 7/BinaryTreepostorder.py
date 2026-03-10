3b497130ff74081fba45486a68b4fc5d34e1281a
Divya-T-K <thangarajdivya23@gmail.com>
2026-03-10 10:24:13 +0530

    first commit

---
+++ Day 7/BinaryTreepostorder.py
@@ -0,0 +1,13 @@
+class Solution(object):
+
+    def postorder(self, root, result):
+        if root is None:
+            return
+        self.postorder(root.left, result)
+        self.postorder(root.right, result)
+        result.append(root.val)
+
+    def postorderTraversal(self, root):
+        result = []
+        self.postorder(root, result)
+        return result
